8f0178facd378a508cc034b00b53a3ad503a9a7c
ZhongMing <734719526@qq.com>
2021-08-21 17:57:24 +0800

    基础框架修改

--- UnityGameBase/Assets/Scrpits/Test/Test.cs
+++ UnityGameBase/Assets/Scrpits/Test/Test.cs
@@ -10,44 +10,30 @@ using UnityEngine.UI;
 
 public class Test : BaseMonoBehaviour
 {
-    public Dictionary<Vector3, string> dicData;
-    string[] listData;
+
     private void OnGUI()
     {
-        if (GUILayout.Button("输出"))
+        if (GUILayout.Button("Test"))
         {
-            Stopwatch stopwatch = TimeUtil.GetMethodTimeStart();
-            for (int i = 0; i < 10000; i++)
-            {
-                string data1 = listData[50000];
-            }
-            TimeUtil.GetMethodTimeEnd("list", stopwatch);
-
-            Stopwatch stopwatch2 = TimeUtil.GetMethodTimeStart();
-            for (int i = 0; i < 10000; i++)
-            {
-                dicData.TryGetValue(new Vector3(55, 0, 0), out string data2);
-            }
-            TimeUtil.GetMethodTimeEnd("dicData", stopwatch2);
-            LogUtil.Log("listData:" + listData.Length);
-            LogUtil.Log("Count:" + dicData.Count);
+            EventHandler.Instance.TriggerEvent("Test");
+            EventHandler.Instance.TriggerEvent("Test2","sadfsaf");
         }
+
     }
 
     private void Start()
     {
-        int cout = 9999999;
-        dicData = new Dictionary<Vector3, string>();
-        listData = new string[cout];
-        for (int i = 0; i < cout; i++)
-        {
-            listData[i] = $"{i}";
-            if (i < 100)
-            {
-                dicData.Add(new Vector3(i, 0, 0), $"{i}");
-            }
-        }
+        EventHandler.Instance.RegisterEvent("Test", TestEvent);
+        EventHandler.Instance.RegisterEvent<string>("Test2", TestEvent2);
     }
 
+    public void TestEvent()
+    {
+        LogUtil.Log("1");
+    }
 
+    public void TestEvent2(string data)
+    {
+        LogUtil.Log("1"+data);
+    }
 }
