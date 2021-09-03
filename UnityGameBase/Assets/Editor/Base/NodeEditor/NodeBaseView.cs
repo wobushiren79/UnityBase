1b75cfbfcb9089fc3dbd279a9b8ce12192c46210
ZhongMing <734719526@qq.com>
2021-09-03 18:39:00 +0800

    节点增加

---
+++ UnityGameBase/Assets/Editor/Base/NodeEditor/NodeBaseView.cs
@@ -0,0 +1,67 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class NodeBaseView
+{
+    public Rect rect;
+    public string title;
+    public bool isDragged;
+
+    public GUIStyle style;
+
+    public NodeBaseView(Vector2 position, float width, float height, GUIStyle nodeStyle)
+    {
+        rect = new Rect(position.x, position.y, width, height);
+        style = nodeStyle;
+    }
+
+    public void Drag(Vector2 delta)
+    {
+        rect.position += delta;
+    }
+
+    public void Draw()
+    {
+        GUI.Box(rect, title, style);
+    }
+
+    /// <summary>
+    /// UI操作处理
+    /// </summary>
+    /// <param name="e"></param>
+    /// <returns></returns>
+    public bool ProcessEvents(Event e)
+    {
+        switch (e.type)
+        {
+            case EventType.MouseDown:
+                if (e.button == 0)
+                {
+                    if (rect.Contains(e.mousePosition))
+                    {
+                        isDragged = true;
+                        GUI.changed = true;
+                    }
+                    else
+                    {
+                        GUI.changed = true;
+                    }
+                }
+                break;
+
+            case EventType.MouseUp:
+                isDragged = false;
+                break;
+
+            case EventType.MouseDrag:
+                if (e.button == 0 && isDragged)
+                {
+                    Drag(e.delta);
+                    e.Use();
+                    return true;
+                }
+                break;
+        }
+        return false;
+    }
+}
