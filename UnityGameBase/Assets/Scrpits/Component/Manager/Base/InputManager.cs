35589f1fc4dd2a6e18f595dcb308802db349dd4c
Apple <734719526@qq.com>
2021-10-07 17:43:53 +0800

    暂存

--- UnityGameBase/Assets/Scrpits/Component/Manager/Base/InputManager.cs
+++ UnityGameBase/Assets/Scrpits/Component/Manager/Base/InputManager.cs
@@ -1,11 +1,45 @@
-﻿using UnityEditor;
+﻿using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputManager : BaseManager
 {
     public GameInputActions inputActions;
+
+    public Dictionary<InputActionUIEnum, InputAction> dicInputUI = new Dictionary<InputActionUIEnum, InputAction>();
+    public Dictionary<string, InputAction> dicInputPlayer = new Dictionary<string, InputAction>();
+
     public virtual void Awake()
     {
         inputActions = new GameInputActions();
+    }
+
+    /// <summary>
+    /// 获取UI数据
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    public InputAction GetInputUIData(InputActionUIEnum name)
+    {
+        if (dicInputUI.TryGetValue(name, out InputAction value))
+        {
+            return value;
+        }
+        return null;
+    }
+
+    /// <summary>
+    /// 获取Player数据
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    public InputAction GetInputPlayerData(string name)
+    {
+        if (dicInputPlayer.TryGetValue(name, out InputAction value))
+        {
+            return value;
+        }
+        return null;
     }
 }
