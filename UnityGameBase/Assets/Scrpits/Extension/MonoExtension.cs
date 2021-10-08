0c37308a99b15e0192d17000a769e27f132920aa
Apple <734719526@qq.com>
2021-10-08 23:11:41 +0800

    基础框架

--- UnityGameBase/Assets/Scrpits/Extension/MonoExtension.cs
+++ UnityGameBase/Assets/Scrpits/Extension/MonoExtension.cs
@@ -11,7 +11,7 @@ public static class MonoExtension
     /// <param name="mono"></param>
     /// <param name="seconds"></param>
     /// <param name="action"></param>
-    public static void DelayExecuteSeconds(this MonoBehaviour mono, float seconds, Action action)
+    public static void WaitExecuteSeconds(this MonoBehaviour mono, float seconds, Action action)
     {
         mono.StartCoroutine(CoroutineForDelayExecuteSeconds(seconds, action));
     }
@@ -22,10 +22,33 @@ public static class MonoExtension
     /// <param name="mono"></param>
     /// <param name="seconds"></param>
     /// <param name="action"></param>
-    public static void DelayExecuteSecondsRealtime(this MonoBehaviour mono, float seconds, Action action)
+    public static void WaitExecuteSecondsRealtime(this MonoBehaviour mono, float seconds, Action action)
     {
         mono.StartCoroutine(CoroutineForDelayExecuteSecondsRealtime(seconds, action));
     }
+
+    /// <summary>
+    /// 延迟执行-当前帧结束
+    /// </summary>
+    /// <param name="mono"></param>
+    /// <param name="numberFrame"></param>
+    /// <param name="action"></param>
+    public static void WaitExecuteEndOfFrame(this MonoBehaviour mono, int numberFrame, Action action)
+    {
+        mono.StartCoroutine(CoroutineForDelayExecuteEndOfFrame(numberFrame, action));
+    }
+
+    /// <summary>
+    /// 延迟执行-fixedUpdate
+    /// </summary>
+    /// <param name="mono"></param>
+    /// <param name="numberFix"></param>
+    /// <param name="action"></param>
+    public static void WaitExecuteFixedUpdate(this MonoBehaviour mono, int numberFix, Action action)
+    {
+        mono.StartCoroutine(CoroutineForDelayExecuteFixedUpdate(numberFix, action));
+    }
+
 
     public static IEnumerator CoroutineForDelayExecuteSeconds(float timeWait, Action action)
     {
@@ -35,6 +58,23 @@ public static class MonoExtension
     public static IEnumerator CoroutineForDelayExecuteSecondsRealtime(float timeWait, Action action)
     {
         yield return new WaitForSecondsRealtime(timeWait);
+        action?.Invoke();
+    }
+
+    public static IEnumerator CoroutineForDelayExecuteEndOfFrame(int numberFrame, Action action)
+    {
+        for (int i = 0; i < numberFrame; i++)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+        action?.Invoke();
+    }
+    public static IEnumerator CoroutineForDelayExecuteFixedUpdate(int numberFix, Action action)
+    {
+        for (int i = 0; i < numberFix; i++)
+        {
+            yield return new WaitForFixedUpdate();
+        }
         action?.Invoke();
     }
 }
