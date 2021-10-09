91c4653b1fe6319a648ee6e44f7a44f0b99f8b71
ZhongMing <734719526@qq.com>
2021-10-09 18:02:39 +0800

    框架优化

--- UnityGameBase/Assets/Scrpits/Test/Test.cs
+++ UnityGameBase/Assets/Scrpits/Test/Test.cs
@@ -1,24 +1,11 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Test : BaseMonoBehaviour
 {
-
     private void OnGUI()
     {
-        if (GUILayout.Button("Create"))
-        {
-            UIHandler.Instance.OpenUIAndCloseOther<UITest>(UIEnum.Test);
-        }
-        if (GUILayout.Button("Add"))
-        {
-            GameConfigBean gameConfig = GameDataHandler.Instance.manager.GetGameConfig();
-            gameConfig.uiSize += 0.1f;
-            if (gameConfig.uiSize >= 2)
-            {
-                gameConfig.uiSize = 0.5f;
-            }
-            UIHandler.Instance.ChangeUISize(gameConfig.uiSize);
-        }
+
     }
 
     private void Start()
